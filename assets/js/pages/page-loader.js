8a9e7a2cf0b83b9b9b748ae599fb1bd6ec9d330d
TheBacou <69599039+TheBacou@users.noreply.github.com>
2024-09-22 16:51:18 +0200

    Update page-loader.js

--- assets/js/pages/page-loader.js
+++ assets/js/pages/page-loader.js
@@ -4,7 +4,7 @@ var file_name = path[path.length - 1]
 var pages = {
     "loading": {
         "js": "./loading.js", // path to the js file
-        "html": "./../loading.html" // path to the html file
+        "html": "loading.html" // path to the html file
     }
 }
 
@@ -12,7 +12,11 @@ var module = {};
 
 (async () => {
     try {
+
         Object.assign(module, await import(`./${pages.loading.js}`)); // adding every fonction of "pages.loading.js" in module with other fonction fetched
+
+        module.loading(pages.loading.html) // send loading page to user
+
     } catch (error) {
         console.error("Erreur lors du chargement du module JS", error);
     }
